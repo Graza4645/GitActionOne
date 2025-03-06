9d53cc3b57ead0141d0027564bd6c3b0d4be3283
Graza4645 <graza4645@gamail.com>
2025-03-06 12:21:08 +0530

    /

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,6 +4,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   console.log('hello world')
+  expect('hello').toBe('h');
 
 
 });
